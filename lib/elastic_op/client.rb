cd474480bfa45d3f77fcef3fb3ccb0c906d9b021
Thomas Tych <thomas.tych@gmail.com>
2025-11-19 23:44:52 +0100

    client as a proxy

--- lib/elastic_op/client.rb
+++ lib/elastic_op/client.rb
@@ -46,5 +46,17 @@ module ElasticOp
         log: logger
       )
     end
+
+    def method_missing(method_name, *args, &block)
+      if client.respond_to?(method_name)
+        client.public_send(method_name, *args, &block)
+      else
+        super
+      end
+    end
+
+    def respond_to_missing?(method_name, include_private = false)
+      client.respond_to?(method_name, include_private) || super
+    end
   end
 end
